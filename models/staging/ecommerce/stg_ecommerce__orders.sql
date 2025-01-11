d210834c7ecd5ed71b6b57057962b844a4b5c6d3
Corinne Richard <corinne.richard100@gmail.com>
2025-01-11 20:40:43 +0000

    add ecommerce source and orders stg

---
+++ models/staging/ecommerce/stg_ecommerce__orders.sql
@@ -0,0 +1,13 @@
+with source as (
+    {{source('ecommerce', 'orders')}}
+)
+
+select order_id,
+    customer_id, 
+    order_status,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_carrier_date,
+    order_delivered_customer_date,
+    order_estimated_delivery_date
+from source
